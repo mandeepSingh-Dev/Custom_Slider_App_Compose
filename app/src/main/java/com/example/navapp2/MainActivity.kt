303ab7e2372b456287ba7ea92b22da645e825170
Mandeep Singh <mandeep.singh@appsinvo.com>
2024-05-03 00:04:06 +0530

    Trapezode shape added

--- app/src/main/java/com/example/navapp2/MainActivity.kt
+++ app/src/main/java/com/example/navapp2/MainActivity.kt
@@ -3,42 +3,44 @@ package com.example.navapp2
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.*
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.interaction.DragInteraction
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
+import androidx.graphics.shapes.CornerRounding
+import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.toPath
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.navapp2.ui.theme.NavApp2Theme
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
@@ -55,19 +57,67 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    
+                    ScreenOne(onNavigate = {})
+/*
                     NavHost(navController = navController, startDestination = "Screen1"){
-                        composable(route= "Screen1"){
-                            ScreenOne( onNavigate = {
+
+                        composable(
+                            route= "Screen1",
+                            enterTransition = {
+                                fadeIn(
+                                    animationSpec = tween(
+                                        300, easing = LinearEasing
+                                    )
+                                ) + slideIntoContainer(
+                                    animationSpec = tween(300, easing = EaseIn),
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Start
+                                )
+                            },
+                            exitTransition = {
+                                fadeOut(
+                                    animationSpec = tween<Float>(
+                                        300, easing = LinearEasing
+                                    )
+                                ) + slideOutOfContainer(
+                                    animationSpec = tween(300, easing = EaseOut),
+                                    towards = AnimatedContentTransitionScope.SlideDirection.End
+                                )
+                            })
+                        {
+                            ScreenOne(onNavigate = {
                                 navController.navigate(it)
                             })
                         }
-                        
-                        composable(route = "Screen2"){
-                            ScreenTwo(){
+                        composable(
+                            route= "Screen1",
+                            enterTransition = {
+                                fadeIn(
+                                    animationSpec = tween(
+                                        300, easing = LinearEasing
+                                    )
+                                ) + slideIntoContainer(
+                                    animationSpec = tween(300, easing = EaseIn),
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Start
+                                )
+                            },
+                            exitTransition = {
+                                fadeOut(
+                                    animationSpec = tween<Float>(
+                                        300, easing = LinearEasing
+                                    )
+                                ) + slideOutOfContainer(
+                                    animationSpec = tween(300, easing = EaseOut),
+                                    towards = AnimatedContentTransitionScope.SlideDirection.End
+                                )
+                            }){
+                            ScreenTwo(onNavigate = {
                                 navController.navigate(it)
-                            }
+                            })
                         }
+
                     }
+*/
                 }
             }
         }
@@ -77,7 +127,109 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ScreenOne( onNavigate : (String) -> Unit) {
-    moveImage()
+//    Text(text = "Helliddfjdfj", modifier = Modifier.clickable { onNavigate("Screen2") })
+
+
+    Box(
+        modifier = Modifier
+            .padding(
+                30.dp
+            )
+            .drawWithCache {
+
+
+                val rect = Rect(Offset.Zero, size)
+
+                val path = androidx.compose.ui.graphics
+                    .Path()
+                    .apply {
+                        moveTo(0f, 0f)
+                        //For left card
+//                        lineTo(0f, 600f)
+//                        lineTo( 500f, 600 * 0.85f,)
+//                        lineTo( 500f, 600 * 0.25f)
+//                        For right card
+
+                        val  topLeftX = 100f;
+                        val  topLeftY = 100f;
+                        val  topRightX = 300f;
+                        val  topRightY = 100f;
+                        val  bottomRightX = 400f;
+                        val  bottomRightY = 300f;
+                        val  bottomLeftX = 50f;
+                        val  bottomLeftY = 300f;
+
+                        // Create a Path object
+
+                        // Move to starting point
+                        moveTo(topLeftX, topLeftY);
+
+                        // Add lines to form the trapezoid
+                        lineTo(topRightX, topRightY);
+                        lineTo(bottomRightX, bottomRightY);
+                        lineTo(bottomLeftX, bottomLeftY);
+
+                      /*  lineTo( 500f, 450f,)
+                        lineTo( 500f, 138f)*/
+                        close()
+                    }
+
+                val paint = Paint().apply {
+                    shader = LinearGradientShader(
+                        rect.topLeft,
+                        rect.bottomRight,
+                        listOf(Color.Red, Color.Blue)
+                    )
+                    pathEffect = PathEffect.cornerPathEffect(60f)
+                }
+
+                onDrawBehind {
+
+                    drawIntoCanvas { canvas ->
+                        canvas.drawOutline(
+                            outline = Outline.Generic(path),
+                            paint = paint
+                        )
+                    }
+
+                }
+
+
+                /*   val path = Path()
+                path.moveTo(0f, 0f)
+                path.lineTo(0f, size.height/2)
+                path.lineTo(size.width/2, size.height/7)
+                path.moveTo(0f, 200f)
+                path.close()
+                onDrawBehind {
+                    drawPath(path, Color.Magenta, style = Stroke(
+                        width = 2.dp.toPx(),
+                        pathEffect = PathEffect.cornerPathEffect(40.dp.toPx())
+                    ))
+                }*/
+
+
+                /* val roundedPolygon = RoundedPolygon(
+                    numVertices = 5,
+                    radius = size.minDimension / 2,
+                    centerX = size.width / 2,
+                    centerY = size.height / 2,
+                    rounding = CornerRounding(
+                        size.minDimension / 10f,
+                        smoothing = 0.1f
+                    )
+                )
+                val roundedPolygonPath = roundedPolygon
+                    .toPath()
+                    .asComposePath()
+                onDrawBehind {
+                    drawPath(roundedPolygonPath, color = Color.Black)
+                }*/
+            }.graphicsLayer {
+                rotationX = 90f
+            }
+    )
+    
 }
 @Composable
 fun ScreenTwo(  onNavigate : (String) -> Unit) {
@@ -93,7 +245,6 @@ fun ScreenTwo(  onNavigate : (String) -> Unit) {
     }
 
     Text(text = "Hello Greetings Screen Two!", modifier = Modifier
-        .scale(r, r)
         .clickable {
             t = 0f
             onNavigate("Screen1")
@@ -146,7 +297,7 @@ Log.d("fvjfnvfkjvnf",screenWidth.toString())
         }
 
         .background(Color.Yellow)) {
-
+        
             Button(
                 onClick = {},
                 modifier = Modifier
@@ -179,7 +330,9 @@ Log.d("fvjfnvfkjvnf",screenWidth.toString())
 
 
 
-                            nWidth = width - (buttonWidth.toFloat().toPx(context)).toInt()
+                            nWidth = width - (buttonWidth
+                                .toFloat()
+                                .toPx(context)).toInt()
                             val newX = (offsetX + dragAmount.x)
 
                             Log.d("fvlmfkvfmlk", width.toString() + " width")
